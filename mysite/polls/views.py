3176add11799103a057e9e3d96f34bbb5d867da9
ASUR12ZURVAN <143814906+ASUR12ZURVAN@users.noreply.github.com>
2025-02-23 17:52:17 +0530

    Modified views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index (request):
-    return HttpResponse("Welcome to INDEX")
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
+    output = ",".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 # Create your views here.
